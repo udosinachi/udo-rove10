a424282d4610b5034990fbfc626e4ef77275c862
Udosinachi <62754095+udosinachi@users.noreply.github.com>
2024-09-11 07:51:17 +0100

    Signup update

--- pages/signup.vue
+++ pages/signup.vue
@@ -72,7 +72,10 @@
               class="block w-full rounded-md border-0 p-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
             />
           </div>
-          <p v-if="password.length <= 7" class="mt-2 text-sm text-red-600">
+          <p
+            v-if="password && password.length <= 7"
+            class="mt-2 text-sm text-red-600"
+          >
             Passwords must contain at least 8 characters
           </p>
 
